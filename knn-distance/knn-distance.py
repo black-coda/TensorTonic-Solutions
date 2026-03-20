a217b62b70be9e1dfb66ca72a93123d9e2e7b9de
Monday Solomon <83866674+black-coda@users.noreply.github.com>
2026-03-20 21:08:23 +0100

    Add solution for knn-distance

---
+++ knn-distance/knn-distance.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def knn_distance(X_train, X_test, k):
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+
+    # Fix 1D case
+    if X_train.ndim == 1:
+        X_train = X_train.reshape(-1, 1)
+    if X_test.ndim == 1:
+        X_test = X_test.reshape(-1, 1)
+
+    # Compute distances
+    train_sq = np.sum(X_train**2, axis=1)
+    test_sq = np.sum(X_test**2, axis=1)
+
+    distances = test_sq[:, np.newaxis] + train_sq - 2 * X_test @ X_train.T
+
+    # Get sorted indices
+    sorted_idx = np.argsort(distances, axis=1)
+
+    n_train = X_train.shape[0]
+
+    if k <= n_train:
+        return sorted_idx[:, :k]
+    else:
+        # Take all available neighbors
+        result = sorted_idx
+
+        # Pad with -1
+        pad_width = k - n_train
+        padding = -1 * np.ones((X_test.shape[0], pad_width), dtype=int)
+
+        return np.hstack([result, padding])
